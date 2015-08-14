cccefe2a56def0c566e0f32b2f8bf5da457971f3
oskar.anderstal@cloudnine.se <oskar.anderstal@cloudnine.se>
2015-08-14 12:24:21 +0200

    shuuu

---
+++ TheTool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheTool.MvcApplication" Language="C#" %>
---
+++ TheTool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheTool.Startup))]
+namespace TheTool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
